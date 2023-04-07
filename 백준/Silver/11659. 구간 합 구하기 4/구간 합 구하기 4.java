48ebd1e2a6cb17eff48081743fd1626ffab53a39
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2023-04-07 13:45:10 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2240 ms, Memory: 80544 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,38 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.Arrays;
-import java.util.Scanner;
+public class Main {
+	static long[] tree;
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st=new StringTokenizer(bf.readLine());
+		
+		int n,m;
+		n=Integer.parseInt(st.nextToken());
+		m=Integer.parseInt(st.nextToken());
 
-public class Main{
-	static int n,m;
-	static int[] arr;
-	static int[][] range;
-	static int[] sum;
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		st=new StringTokenizer(bf.readLine());
+		int[] arr=new int[n+1];
+		tree=new long[n+1];
 		
-		n=sc.nextInt();
-		m=sc.nextInt();
 		
-		arr=new int[n];
-		sum=new int[n+1];
-		range=new int[m][2];
+		int[][] op=new int[m][2];
 		
-		for(int i=0;i<n;i++) {
-			arr[i]=sc.nextInt();
-		}
 		
-		for(int i=1;i<n+1;i++) {
-			sum[i]=sum[i-1]+arr[i-1];
+		for(int i=1;i<=n;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+			update(i,arr[i]);
 		}
 		
 		for(int i=0;i<m;i++) {
-			for(int j=0;j<2;j++) {
-				range[i][j]=sc.nextInt();
-			}
+			st=new StringTokenizer(bf.readLine());
+			op[i][0]=Integer.parseInt(st.nextToken());
+			op[i][1]=Integer.parseInt(st.nextToken());
 		}
+		
 		for(int i=0;i<m;i++) {
-			System.out.println(sum[range[i][1]]-sum[range[i][0]-1]);
+			System.out.println(sum(op[i][1])-sum(op[i][0]-1));
 		}
+		
+		
+	}
+	private static long sum(int idx) {
+		long sum=0;
+		while(idx>0) {
+			sum+=tree[idx];
+			idx -= (idx&-idx);
+		}
+		return sum;
+	}
+	private static void update(int idx, int val) {
+		while(idx<tree.length) {
+			tree[idx]+=val;
+			idx+=(idx&-idx);
+		}
+		
 	}
 
 }
