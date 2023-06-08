a19321cdbbd8a7fe7f4cc92859baa87f1fb10011
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2023-06-08 21:14:48 +0900

    [Gold V] Title: 기차 여행, Time: 108 ms, Memory: 12344 KB -BaekjoonHub

--- 백준/Gold/10713. 기차 여행/기차 여행.java
+++ 백준/Gold/10713. 기차 여행/기차 여행.java
@@ -3,14 +3,6 @@ import java.io.*;
 
 public class Main {
     static int n,m;
-    static class info{
-        int destination;
-        int trainNumber;
-        public info(int destination,int trainNumber){
-            this.destination=destination;
-            this.trainNumber=trainNumber;
-        }
-    }
     static class trainPrice{
         int ticket;
         int ic;
@@ -24,7 +16,6 @@ public class Main {
     static int plan[];
     static trainPrice train[];
     static int trainCnt[];
-    static ArrayList<info> arr[];
     public static void main(String[] args) throws Exception{
         BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(bf.readLine());
@@ -35,19 +26,6 @@ public class Main {
         plan=new int[m]; //j일 째, Pj -> Pj+1로 이동
         train=new trainPrice[n-1];
         trainCnt=new int[n-1]; //해당 철도 타는 횟수
-        arr=new ArrayList[n];
-
-//        for(int i=0;i<n;i++){
-//            arr[i]=new ArrayList<>();
-//            if(i>=1 && i<n-1) {
-//                arr[i].add(new info(i + 1, i));
-//                arr[i].add(new info(i-1,i-1));
-//            }else if(i==0){
-//                arr[i].add(new info(i+1,i));
-//            }else{
-//                arr[i].add(new info(i-1,i-1));
-//            }
-//        }
 
 
         st=new StringTokenizer(bf.readLine());
@@ -67,10 +45,10 @@ public class Main {
         for(int i=0;i<m-1;i++){
             cal(plan[i]-1,plan[i+1]-1);
         }
-        int ans=0;
+        long ans=0;
         for(int i=0;i<trainCnt.length;i++){
-            int ticket=trainCnt[i]*train[i].ticket;
-            int card=(trainCnt[i]*train[i].ic)+train[i].card;
+            long ticket=trainCnt[i]*train[i].ticket;
+            long card=(trainCnt[i]*train[i].ic)+train[i].card;
             ans+=Math.min(ticket,card);
         }
         System.out.println(ans);
