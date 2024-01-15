adcecdebe8cedc9f3d2db06a6b378019cc28ca71
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-01-15 14:16:44 +0900

    [Gold IV] Title: 트리, Time: 440 ms, Memory: 63832 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.java
+++ 백준/Gold/4803. 트리/트리.java
@@ -1,186 +1,126 @@
-import java.sql.Array;
 import java.util.*;
 import java.io.*;
+
 public class Main {
+    static int N,M;
+    static int[] ch;
     static int[] parent;
-    static ArrayList<String> ans=new ArrayList<>();
-    static class node implements Comparable<node>{
+    static class Node implements Comparable<Node>{
         int n1;
         int n2;
-        public node (int n1,int n2){
+        int d;
+        public Node(int n1,int n2,int d){
             this.n1=n1;
             this.n2=n2;
+            this.d=d;
         }
-
         @Override
-        public int compareTo(node o){
-            int x=this.n1-o.n1;
-            if(x==0){
-                x=this.n2-o.n2;
-            }
-            return x;
+        public int compareTo(Node o){
+            return this.d-o.d;
         }
-
     }
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder answer=new StringBuilder();
         int t=1;
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        while(true) {
-
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            HashMap<Integer, Integer> map = new HashMap<>();
-            HashMap<Integer, Integer> nomap = new HashMap<>();
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            if (n == 0 && m == 0) {
+        while(true){
+            int ans=0;
+            HashSet<Integer> tmp=new HashSet<>();
+            st=new StringTokenizer(bf.readLine());
+            N=Integer.parseInt(st.nextToken());
+            M=Integer.parseInt(st.nextToken());
+            if(N==0 && M==0){
                 break;
             }
-            parent = new int[n + 1];
-            for (int i = 0; i < n + 1; i++) {
-                parent[i] = i;
+            ch=new int[N+1];
+            parent=new int[N+1];
+            PriorityQueue<Node> pq=new PriorityQueue<>();
+            for(int i=1;i<N+1;i++){
+                parent[i]=i;
             }
-            ArrayList<node> list=new ArrayList<>();
-            for(int i=0;i<m;i++){
+            for(int i=0;i<M;i++){
                 st=new StringTokenizer(bf.readLine());
                 int n1=Integer.parseInt(st.nextToken());
                 int n2=Integer.parseInt(st.nextToken());
-                if(n1>n2){
-                    list.add(new node(n2,n1));
-                }else{
-                    list.add(new node(n1,n2));
+                pq.add(new Node(n1,n2,1));
+            }
+            while(!pq.isEmpty()){
+                Node now=pq.poll();
+                int n1=now.n1;
+                int n2=now.n2;
+                if(findParent(n1)!=findParent(n2)){
+                    union(n1,n2);
+                }
+                else{
+                    int parent=findParent(n1);
+                    ch[parent]=1;
                 }
             }
-            Collections.sort(list);
-            for(int i=0;i<list.size();i++){
-                if(!union(list.get(i).n1,list.get(i).n2)){
-                    nomap.put(getParent(list.get(i).n1),1);
+            for(int i=1;i<N+1;i++){
+                int parent=findParent(i);
+                tmp.add(parent);
+            }
+            for(int val:tmp){
+                if(ch[val]==0){
+                    ans++;
                 }
-
             }
-            for(int i=1;i<n+1;i++){
-                map.put(getParent(i),1);
+            if(ans==1){
+                answer.append("Case "+t+": There is one tree.\n");
             }
-//            for(Integer key:map.keySet()){
-//                System.out.println(key);
-//            }
-            for(Integer key:nomap.keySet()){
-                map.remove(key);
+            else if(ans==0){
+                answer.append("Case "+t+": No trees.\n");
             }
-            if(map.size()==0){
-                ans.add("Case "+t+": No trees.");
-            }else if(map.size()==1){
-                ans.add("Case "+t+": There is one tree.");
-            }else{
-                ans.add("Case "+t+": A forest of "+map.size()+" trees.");
+            else{
+                answer.append("Case "+t+": A forest of "+ans+" trees.\n");
             }
             t++;
         }
-        for(int i=0;i<ans.size();i++){
-            System.out.println(ans.get(i));
+        bw.write(answer.toString());
+        bw.flush();
+        bw.close();
+    }
+    public static int findParent(int x){
+        if(x==parent[x]){
+            return x;
+        }
+        return findParent(parent[x]);
+    }
+    public static void union(int a,int b){
+        int parentA=findParent(a);
+        int parentB=findParent(b);
+        if(parentA==parentB){
+            return ;
+        }
+        if(parentA<parentB){
+            parent[parentB]=findParent(parentA);
+        }
+        else{
+            parent[parentA]=findParent(parentB);
         }
     }
-    public static int getParent(int x){
-        if(parent[x]!=x){
-            return getParent(parent[x]);
-        }
-        return x;
-    }
-    public static boolean union(int a,int b){
-        int parentA=getParent(a);
-        int parentB=getParent(b);
-        if(parentA!=parentB){
-            if(parentA>parentB){
-                parent[parentA]=parentB;
-
-            }else{
-                parent[parentB]=parentA;
-            }
-            return true;
-        }else{
-            return false;
-        }
-    }
-
 
 }
-
 /*
-3 3
-1 2
-3 1
-3 2
-0 0
--> no
-
-7 7
-1 2
-2 3
-3 1
-4 5
-5 6
-6 4
-1 6
--> one
-
-4 4
-2 3
-3 4
-4 2
-1 2
-0 0
--> no
-
-6 7
-1 2
-1 3
-1 4
-2 3
-2 4
-3 4
-5 6
-0 0
--> one
-
-6 4
-5 6
-3 4
 6 3
-5 4
--> 2
-
+1 2
+2 3
+3 4
+---
 6 5
-5 6
-3 4
-6 3
-6 1
-5 4
--> 1
-
-6 5
-5 6
-3 4
-6 3
-1 3
-6 2
-->1
-
-9 9
 1 2
 2 3
 3 4
 4 5
-3 5
-6 7
-7 8
-6 8
-8 9
-정답 : Case 1: No trees.
-
+5 6
+---
 6 6
-5 6
-3 4
-6 3
+1 2
+2 3
 1 3
-6 2
-1 6
+4 5
+5 6
+6 4
 * */
