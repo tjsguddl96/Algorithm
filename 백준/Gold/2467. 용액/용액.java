9d0f7c26add1bdec69e11f084cfdde8eb5abb63c
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-02-25 21:07:42 +0900

    [Gold V] Title: 용액, Time: 316 ms, Memory: 34524 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,77 +1,81 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-	static int n;
-	static int[] answer;
-	static ArrayList<Integer> posi;
-	static ArrayList<Integer> nega;
-	static int ans;
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-		n=Integer.parseInt(bf.readLine());
-		
-		answer=new int[2];
-		posi=new ArrayList<>();
-		nega=new ArrayList<>();
-		
-		ans=Integer.MAX_VALUE;
-		StringTokenizer st=new StringTokenizer(bf.readLine());
-		for(int i=0;i<n;i++) {
-			int a=Integer.parseInt(st.nextToken());
-			if(a>=0) {
-				posi.add(a);
-			}else {
-				nega.add(a);
-			}
-		}
-		for(int i=0;i<nega.size();i++) {
-			binary(nega.get(i));
-		}
-		if(nega.size()>=2) {
-			int size=nega.size();
-			int tmp=Math.abs(nega.get(size-1)+nega.get(size-2));
-			if(tmp<ans) {
-				ans=tmp;
-				answer[0]=nega.get(size-1);
-				answer[1]=nega.get(size-2);
-			}
-		}
-		if(posi.size()>=2) {
-			int tmp=posi.get(0)+posi.get(1);
-			if(tmp<ans) {
-				ans=tmp;
-				answer[0]=posi.get(0);
-				answer[1]=posi.get(1);
-			}
-		}
-		Arrays.sort(answer);
-		System.out.println(answer[0]+" "+answer[1]);
-	}
-	public static void binary(int target) {
-		int left=0;
-		int right=posi.size()-1;
-		while(left<=right) {
-			int mid=(left+right)/2;
-			int tmp=target+posi.get(mid);
-			if(ans>Math.abs(tmp)) {
-				ans=Math.abs(tmp);
-				answer[0]=target;
-				answer[1]=posi.get(mid);
-			}
-			if(tmp<=0) {
-				left=mid+1;
-			}
-			else {
-				right=mid-1;
-			}
-			
-		}
-		
-	}
+    static int N;
+    static ArrayList<Integer> positive=new ArrayList<>();
+    static ArrayList<Integer> negative=new ArrayList<>();
+    static int[] answer=new int[2];
+    static int ans=Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        N=Integer.parseInt(bf.readLine());
+        StringTokenizer st=new StringTokenizer(bf.readLine());
+
+        for(int i=0;i<N;i++){
+            int n=Integer.parseInt(st.nextToken());
+            if(n<0){
+                negative.add(n);
+            }
+            else{
+                positive.add(n);
+            }
+        }
+
+
+        for(int i=0;i<negative.size();i++){
+            binary(negative.get(i));
+        }
+
+        if(positive.size()>=2){
+            if(ans> positive.get(0)+ positive.get(1)){
+                ans=positive.get(0)+positive.get(1);
+                answer[0]=positive.get(0);
+                answer[1]=positive.get(1);
+            }
+
+        }
+        if(negative.size()>=2){
+            int s=negative.size();
+            if(ans> Math.abs(negative.get(s-2)+ negative.get(s-1))){
+                answer[0]= negative.get(s-2);
+                answer[1]= negative.get(s-1);
+            }
+        }
+        Arrays.sort(answer);
+        System.out.println(answer[0]+" "+answer[1]);
+
+    }
+    public static void binary(int target){
+        int left=0;
+        int right=positive.size()-1;
+        int mid=0;
+        while(left<=right){
+            mid=(left+right)/2;
+            int tmp=target+positive.get(mid);
+            if(ans>Math.abs(tmp)){
+                ans=Math.abs(tmp);
+                answer[0]=target;
+                answer[1]=positive.get(mid);
+            }
+            if(tmp<=0){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+    }
 }
+/*
+4
+-1 1 2 3
+
+5
+-2 -1 1 2 3
+
+4
+-100 1 2 3
+
+4
+-3 -2 -1 100
+* */
