7f1d41454b4610c9c10e374fee94c19a535b1bc5
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-03-12 20:05:01 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,18 +1,16 @@
--- 코드를 입력하세요
-select b.user_id,b.nickname,a.hap as TOTAL_SALES
-from used_goods_user as b
-    join 
-        (SELECT writer_id,sum(price) as hap
-        from used_goods_board
-        group by status,writer_id
-        having status='DONE' and hap>=700000) as a
-    on b.user_id=a.writer_id
-order by total_sales asc
+select u.user_id as user_id,u.nickname as nickname,sum(b.price) as total_sales
+from used_goods_board as b inner join used_goods_user as u
+    on b.writer_id=u.user_id
+group by b.writer_id,b.status
+having b.status='DONE' and sum(b.price)>=700000
+order by total_sales
 
-# SELECT *
+
+# select writer_id,price,status
 # from used_goods_board
-# where writer_id='dhfkzmf09'
+# order by writer_id
 
-#tkfkdgo1 310000
-#xlaortm1 done:10000,sale:0
-#dhfkzmf09	860000 done
+# select writer_id,sum(price),status
+# from used_goods_board
+# group by writer_id,status
+# order by writer_id
