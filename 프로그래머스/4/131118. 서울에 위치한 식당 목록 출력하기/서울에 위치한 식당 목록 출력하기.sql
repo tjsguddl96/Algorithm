1d10e86a14463bb5e9f0d3965148bba042e21ca1
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-03-12 18:12:42 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,20 +1,15 @@
--- 코드를 입력하세요
-select i.rest_id,i.rest_name,i.food_type,i.favorites,i.address,r.score as score
-from rest_info as i
-    inner join (select rest_id,round(avg(review_score),2) as score
-               from rest_review 
-               group by rest_id) as r
-    on i.rest_id=r.rest_id
-where i.address like "서울%"
-order by score desc,favorites desc
+select i.rest_id as REST_ID	,i.rest_name as REST_NAME,i.food_type as FOOD_TYPE,i.favorites as FAVORITES	,i.address as ADDRESS,v.review_score as SCORE
+from rest_info as i inner join (select rest_id,round(avg(review_score),2) as review_score
+                                from rest_review
+                                group by rest_id) as v
+                    on i.rest_id=v.rest_id
+where i.address like '서울%'
+order by SCORE desc,FAVORITES desc
 
-# select *
+# select * 
 # from rest_info
-# where address like '서울특별시%'
+# where address like '서울%'
 
-
-
-# select rest_id, round(avg(review_score),2)
-# from rest_review
-# group by rest_id
-# having rest_id =00001 or rest_id=00005 or rest_id=00008
+# select v.rest_id as rest_id,round(avg(v.review_score),3) as review_score
+# from rest_review as v
+# group by v.rest_id
