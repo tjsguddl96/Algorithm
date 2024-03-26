b8d21a99bbe7bd9ccadbdd6f3e06011660557f3d
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-03-27 01:54:21 +0900

    [Gold II] Title: 카누 선수, Time: 4412 ms, Memory: 366972 KB -BaekjoonHub

--- 백준/Gold/9007. 카누 선수/카누 선수.java
+++ 백준/Gold/9007. 카누 선수/카누 선수.java
@@ -1,59 +1,108 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int C[][] = new int[5][1000];
-	static int t, n, k, result, sub;
-	static Scanner sc = new Scanner(System.in);
-	
-	public static void main(String[] args) {
-		t = sc.nextInt();
-		for(int i = 0 ; i < t ; i++)
-			System.out.println(B());
- 	}
-	
-	static public int B() {
-		result = sub = Integer.MAX_VALUE;
-		k = sc.nextInt();
-		n = sc.nextInt();
-		ArrayList<Integer> C_12 = new ArrayList<Integer>(n*n);
-		ArrayList<Integer> C_34 = new ArrayList<Integer>(n*n);
-		
-		for(int i = 1 ; i <= 4; i++) {
-			for(int j = 0 ; j < n ; j++) {
-				C[i][j] = sc.nextInt();
-			}
-		}
-		
-		for(int i = 0 ; i < n; i++) {
-			for(int j = 0 ; j < n; j++) {
-				C_12.add(C[1][i]+C[2][j]);
-				C_34.add(C[3][i]+C[4][j]);
-			}
-		}
-		
-		C_34.sort(null);
-				
-		for(int i = 0 ; i < n * n; i++) {
-			int l = n * n - 1;
-			int f = 0;
-			int diff, m, sum;
-			while(l >= f) {
-				m = (l + f) / 2;
-				sum = C_12.get(i) + C_34.get(m);
-				diff = Math.abs(sum - k);
-				
-				if(sum == k) return sum;
-				else if(sum < k) f = m + 1;
-				else l = m - 1;
-				
-				if(diff < sub) {
-					result = sum;
-					sub = diff;
-				} else if(diff == sub) {
-					result = Math.min(result, sum);
-				}
-			}
-		}
-		return result;
-	}
+    static int T;
+    static int k;
+    static int n; //k=특정값,n=각 반의 학생 수
+    static int[][] cl;
+    static BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException{
+        T=Integer.parseInt(bf.readLine());
+        for(int t=0;t<T;t++) {
+            System.out.println(binary());
+        }
+    }
+    public static int binary() throws IOException {
+        StringTokenizer st=new StringTokenizer(bf.readLine());
+        k=Integer.parseInt(st.nextToken());
+        n=Integer.parseInt(st.nextToken());
+        cl=new int[4][n];
+        ArrayList<Integer> sum1=new ArrayList<>();
+        ArrayList<Integer> sum2=new ArrayList<>();
+        for(int i=0;i<4;i++){
+            st=new StringTokenizer(bf.readLine());
+            for(int j=0;j<n;j++){
+                cl[i][j]=Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                sum1.add(cl[0][i]+cl[1][j]);
+                sum2.add(cl[2][i]+cl[3][j]);
+            }
+        }
+        sum2.sort(null);
+        int ans=Integer.MAX_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n*n;i++){
+            int right=n*n-1;
+            int left=0;
+            int mid,temp,diff;
+            while(left<=right){
+                mid=(left+right)/2;
+                temp=sum1.get(i)+sum2.get(mid);
+                diff=Math.abs(k-temp);
+                if(temp==k){
+                    ans=temp;
+                    return ans;
+                }
+                if(diff<min){
+                    min = diff;
+                    ans = temp;
+                }else if(diff==min){
+                    ans=Math.min(ans,temp);
+                }
+                if(temp<k){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
+            }
+        }
+        return ans;
+    }
 }
+/*
+1
+32 2
+5 2
+9 4
+10 20
+4 2
+
+
+3
+300 4
+60 52 80 40
+75 68 88 63
+48 93 48 54
+56 73 49 75
+8 3
+1 2 3
+1 2 3
+1 2 3
+1 2 3
+32 2
+5 2
+9 4
+10 20
+4 2
+
+1
+100 2
+10000000 10000000
+10000000 10000000
+10000000 10000000
+10000000 10000001
+
+
+1
+165 9
+50 50 50 50 50 50 50 50 50
+50 50 50 50 50 50 50 50 50
+2 5 4 5 10 10 20 40 60
+50 55 60 60 70 70 70 60 60
+* */
