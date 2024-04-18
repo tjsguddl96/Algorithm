4f100b429ef75de7d2ec055e5f989e7d335469ae
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-04-18 15:26:36 +0900

    [Gold V] Title: 평범한 배낭, Time: 220 ms, Memory: 54168 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,46 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st=new StringTokenizer(bf.readLine());
-		
-		int n=Integer.parseInt(st.nextToken());
-		int k=Integer.parseInt(st.nextToken());
-		
-		int[][] arr=new int[n+1][2]; //arr[i][0] : 물건 무게, arr[i][1] : 물건 가치
-		int[][] dp=new int[n+1][k+1];
-		
-		
-		for(int i=1;i<n+1;i++) {
-			StringTokenizer st1=new StringTokenizer(bf.readLine());
-			for(int j=0;j<2;j++) {
-				arr[i][j]=Integer.parseInt(st1.nextToken());
-			}
-			
-		}
+    static int N,K;
+    static class Item{
+        int w;
+        int v;
+        public Item(int w,int v){
+            this.w=w;
+            this.v=v;
+        }
+    }
+    static Item[] items;
+    static int[][] dp;
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(bf.readLine());
+        N=Integer.parseInt(st.nextToken());
+        K=Integer.parseInt(st.nextToken());
+        items=new Item[N+1];
+        dp=new int[N+1][100001];
+        for(int i=1;i<N+1;i++){
+            st=new StringTokenizer(bf.readLine());
+            int w=Integer.parseInt(st.nextToken());
+            int v=Integer.parseInt(st.nextToken());
+            items[i]=new Item(w,v);
+        }
 
-		int maxx=0;
-		
-		for(int i=1;i<n+1;i++) {
-			for(int j=1;j<k+1;j++) {
-				if(arr[i][0]<=j) { //넣을수 있는 경우
-					dp[i][j]=Math.max(dp[i-1][j], dp[i-1][j-arr[i][0]]+arr[i][1]);
-				}
-				else {
-					dp[i][j]=dp[i-1][j];
-				}
-			}
-		}
-		System.out.println(dp[n][k]);
-		
-		
-	}
-
+        for(int i=1;i<N+1;i++){
+            Item now=items[i];
+            int nowV=items[i].v;
+            int nowW=items[i].w;
+            dp[i][nowW]=Math.max(nowV,dp[i][nowV]);
+            for(int j=0;j<=K;j++){
+                if(nowW+j<=K){
+                    dp[i][nowW+j]=Math.max(dp[i][nowW+j],dp[i-1][j]+nowV);
+                }
+                dp[i][j]=Math.max(dp[i][j],dp[i-1][j]);
+            }
+        }
+        int max=0;
+        for(int i=0;i<=K;i++){
+            if(max<dp[N][i]){
+                max=dp[N][i];
+            }
+        }
+        System.out.println(max);
+    }
 }
