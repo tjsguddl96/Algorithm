48b7a9f0f280fce6d362d0389bfd342bd47eafa7
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-05-30 15:54:42 +0900

    [Gold IV] Title: Puyo Puyo, Time: 120 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
@@ -1,159 +1,146 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 public class Main {
     static char[][] map=new char[12][6];
-    static int[] dx={0,0,1,-1};
     static int[] dy={1,-1,0,0};
-    static ArrayDeque<Node> q=new ArrayDeque<>();
-    static PriorityQueue<Node> q2=new PriorityQueue<>();
-    static int[][] ch=new int[12][6];
-    static class Node implements Comparable<Node>{
+    static int[] dx={0,0,1,-1};
+    static class Position{
         int y;
         int x;
-        char color;
-        public Node(int y,int x,char color){
+        public Position(int y,int x){
             this.y=y;
             this.x=x;
-            this.color=color;
-        }
-        @Override
-        public int compareTo(Node o){
-            int com=this.y-o.y;
-            return com;
         }
     }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static class Node{
+        int y;
+        int x;
+        char c;
+        int cnt;
+        public Node(int y,int x,char c,int cnt){
+            this.y=y;
+            this.x=x;
+            this.c=c;
+            this.cnt=cnt;
+        }
+    }
+    static int ans;
+    static int flag;
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
         for(int i=0;i<12;i++){
-            String str=br.readLine();
+            String str=bf.readLine();
             for(int j=0;j<6;j++){
                 map[i][j]=str.charAt(j);
             }
         }
-        int answer=0;
-        int flag=0;
-        while(true) {
-            flag = 0;
-            ch=new int[12][6];
-            for (int y = 0; y <12; y++) {
-                for (int x = 0; x < 6; x++) {
-                    if (map[y][x] != '.' && ch[y][x]==0) {
-                        q.add(new Node(y, x, map[y][x]));
-                        int cnt = bfs();
-                        if (cnt >= 4) {
-                            flag = 1;
-                            pop();
-                        } else {
-                            q2 = new PriorityQueue<>();
-                        }
+        while(flag==0) {
+            flag=1;
+            for (int i = 11; i >= 0; i--) {
+                for (int j = 0; j < 6; j++) {
+                    if (map[i][j] != '.') {
+                        bfs(i, j);
                     }
                 }
             }
-            if(flag==1){
-                answer++;
+            if(flag==0){
+                ans++;
             }
-            else if(flag==0){
-                break;
-            }
-        }
-//        int y=11;
-//        int x=0;
-//        while(y>=0){
-//            while(x<6){
-//                if(map[y][x]!='.'){
-//                    q.add(new Node(y,x,map[y][x]));
-//                    int cnt=bfs();
-//                    if(cnt>=4){ //터뜨리고, 위에 있는 애들 밑으로 와야해
-//                        pop();
-//                        y=11;
-//                        x=0;
-//                        answer++;
-//                    }
-//                    else{
-//                        q2=new PriorityQueue<>();
-//                        x++;
-//                    }
-//                }
-//                else{
-//                    x++;
-//                }
-//            }
-//            y--;
-//            x = 0;
-//
-//        }
-
-        System.out.println(answer);
-
-
-    }
-    public static void pop(){
-        while(!q2.isEmpty()){
-            Node now=q2.poll();
-            int nowY=now.y;
-            int nowX=now.x;
-            if(nowY>0) {
-                for (int i = nowY - 1; i >= 0; i--) {
-                    map[i+1][nowX]=map[i][nowX];
+            for(int a=10;a>=0;a--){
+                for(int b=0;b<6;b++){
+                    if(map[a][b]!='.' && map[a+1][b]=='.'){
+                        down(a,b,map[a][b]);
+                    }
                 }
             }
-            map[0][nowX]='.';
         }
-
+        System.out.println(ans);
     }
-    public static int bfs(){
-        int cnt=0;
+    public static void bfs(int y,int x){
+        ArrayDeque<Node> q=new ArrayDeque<>();
+        ArrayDeque<Position> tmp=new ArrayDeque<>();
+        q.add(new Node(y,x,map[y][x],1));
+        char nowC=map[y][x];
+        map[y][x]='.';
+        int nowCnt=1;
         while(!q.isEmpty()){
             Node now=q.poll();
             int nowY=now.y;
             int nowX=now.x;
-            char nowColor=now.color;
-            ch[nowY][nowX]=1;
-            cnt++;
-            q2.add(new Node(nowY,nowX,nowColor));
+            nowC=now.c;
+            tmp.add(new Position(nowY,nowX));
             for(int i=0;i<4;i++){
                 int nextY=nowY+dy[i];
                 int nextX=nowX+dx[i];
-                if(nextY>=0 && nextY<12 && nextX>=0 && nextX<6 && ch[nextY][nextX]==0 && nowColor==map[nextY][nextX]){
-                    q.add(new Node(nextY,nextX,map[nextY][nextX]));
-                    ch[nextY][nextX]=1;
+                if(nextY>=0 && nextY<12 && nextX>=0 && nextX<6 && map[nextY][nextX]==nowC){
+                    map[nextY][nextX]='.';
+                    nowCnt++;
+                    q.add(new Node(nextY,nextX,nowC,nowCnt+1));
                 }
             }
         }
-        return cnt;
+        if(nowCnt<4){
+            while(!tmp.isEmpty()){
+                Position now=tmp.poll();
+                int nowY=now.y;
+                int nowX=now.x;
+                map[nowY][nowX]=nowC;
+            }
+        }
+        else{
+//            for(int i=10;i>=0;i--){
+//                for(int j=0;j<6;j++){
+//                    if(map[i][j]!='.' && map[i+1][j]=='.'){
+//                        down(i,j,map[i][j]);
+//                    }
+//                }
+//            }
+            flag=0;
+//            for(int i=0;i<12;i++){
+//                for(int j=0;j<6;j++){
+//                    System.out.print(map[i][j]+" ");
+//                }
+//                System.out.println();
+//            }
+//            System.out.println("-------------");
+        }
+    }
+    public static void down(int y,int x,char c){
+        int j=y+1;
+        while(j<=11 && map[j][x]=='.'){
+            j++;
+        }
+        map[y][x]='.';
+        map[j-1][x]=c;
+
     }
 }
-
-
 /*
 ......
 ......
 ......
 ......
 ......
-......
-....G.
-....G.
-...RR.
-..GYR.
-RRRRR.
-RRYGGG
-->2
+GG....
+RR....
+YR....
+RR....
+YRY...
+RRRYGG
+GYGRYY
+->1
 
-
 ......
 ......
 ......
 ......
-......
-......
-......
-......
-......
-......
-......
-RBRBRB
-->
-*/
+.G....
+GY....
+RR....
+YR....
+RR....
+YRY...
+RRRYGG
+GYGRYY
+->3
+* */
