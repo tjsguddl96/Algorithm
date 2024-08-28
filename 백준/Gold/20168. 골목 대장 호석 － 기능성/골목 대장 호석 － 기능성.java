ccbe28fa932035357a33ec25d07eeba4b35fb289
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-08-28 14:17:30 +0900

    [Gold V] Title: 골목 대장 호석 - 기능성, Time: 100 ms, Memory: 14300 KB, Score: 18 point -BaekjoonHub

--- 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.java
+++ 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.java
@@ -3,15 +3,33 @@ import java.io.*;
 public class Main {
     static int N,M,A,B,C;
     static int[] d;
-    static class Node{
+    static class Node2{
         int n;
         int w;
-        public Node(int n,int w){
+        public Node2(int n,int w){
             this.n=n;
             this.w=w;
         }
     }
-    static ArrayList<Node>[] arr;
+    static class Node implements Comparable<Node>{
+        int n;
+        int w;
+        int m;
+        public Node(int n,int w,int m){
+            this.n=n;
+            this.w=w;
+            this.m=m;
+        }
+        @Override
+        public int compareTo(Node o){
+            int x=this.m-o.m;
+            if(x==0){
+                x=this.w-o.w;
+            }
+            return x;
+        }
+    }
+    static ArrayList<Node2>[] arr;
     static int answer=Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException{
         BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
@@ -33,32 +51,39 @@ public class Main {
             int a=Integer.parseInt(st.nextToken());
             int b=Integer.parseInt(st.nextToken());
             int c=Integer.parseInt(st.nextToken());
-            arr[a].add(new Node(b,c));
-            arr[b].add(new Node(a,c));
+            arr[a].add(new Node2(b,c));
+            arr[b].add(new Node2(a,c));
         }
-        solve(A,0,0);
+        PriorityQueue<Node> pq=new PriorityQueue<>();
+        pq.add(new Node(A,0,0));
+        while(!pq.isEmpty()){
+            Node now=pq.poll();
+            int nowNode=now.n;
+            int nowD=now.w;
+            int nowMin=now.m;
+            if(C<nowD){
+                continue;
+            }
+            if(nowNode==B){
+                answer=Math.min(answer,nowMin);
+            }
+            for(int i=0;i<arr[nowNode].size();i++){
+                Node2 next=arr[nowNode].get(i);
+                int nextNode=next.n;
+                int nextW=next.w;
+                if(nextW+nowD<=C && d[nextNode]>Math.max(nextW,nowMin)){
+                    d[nextNode]=Math.max(nextW,nowMin);
+                    pq.add(new Node(nextNode,nextW+nowD,d[nextNode]));
+                }
+            }
+        }
         if(answer==Integer.MAX_VALUE){
             answer=-1;
         }
         System.out.println(answer);
     }
-    public static void solve(int now,int money,int min){
-        if(money>C || d[now]<money){
-            return ;
-        }
-        if(now==B){
-            answer=Math.min(min,answer);
-            return ;
-        }
-        for(int i=0;i<arr[now].size();i++){
-            Node next=arr[now].get(i);
-            int nextNode=next.n;
-            int nextW=next.w;
-            if(money+nextW<d[nextNode]){
-                d[nextNode]=money+nextW;
-                solve(nextNode,money+nextW,Math.max(min,nextW));
-            }
-        }
-
-    }
 }
+/*
+3 1 1 3 1
+1 3 2
+* */
