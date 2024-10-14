36237c32f928bf0a7a1ccc750bf3b48d6e44d36b
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-10-14 15:31:53 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 81.02 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,72 +1,78 @@
 import java.util.*;
+
 class Solution {
-    static class Node{
+    static int[] dy={1,-1,0,0};
+    static int[] dx={0,0,1,-1};
+    static class Position{
         int y;
         int x;
-        public Node(int y,int x){
+        public Position(int y,int x){
             this.y=y;
             this.x=x;
         }
+
     }
-    static int[] dy={0,0,-1,1};
-    static int[] dx={1,-1,0,0};
-    static int N,M;//세로 가로
+    static int N,M;//N=행->세로, M=열->가로
     static int[][] ch;
-    static HashMap<Integer,Integer> map=new HashMap<>();
-    public int solution(int[][] land) { //land[i][j]=0 -> 땅, land[i][j]=1 -> 석유
+    static ArrayList<Integer> list=new ArrayList<>();
+    static int[] ch2;
+    public int solution(int[][] land) {
         int answer = 0;
+        list.add(-1);
+        int idx=1;
         N=land.length;
         M=land[0].length;
         ch=new int[N][M];
-        int number=1;
         for(int i=0;i<N;i++){
             for(int j=0;j<M;j++){
                 if(land[i][j]==1 && ch[i][j]==0){
-                    bfs(i,j,number,land);
-                    number++;
+                    bfs(i,j,idx,land);
+                    idx++;
                 }
             }
         }
-        for(int i=0;i<M;i++){ //열
-            HashSet<Integer> set=new HashSet<>();
-            int tmp=0;
-            for(int j=0;j<N;j++){ //행
-                if(ch[j][i]!=0 && !set.contains(ch[j][i])){
-                    set.add(ch[j][i]);
-                    tmp+=map.get(ch[j][i]);
+        
+        for(int i=0;i<M;i++){
+            int sum=0;
+            ch2=new int[idx];
+            for(int j=0;j<N;j++){
+                if(ch2[ch[j][i]]==0 && ch[j][i]!=0){
+                    ch2[ch[j][i]]=1;
+                    sum+=list.get(ch[j][i]);
                 }
             }
-            answer=Math.max(answer,tmp);
-            
+            System.out.println();
+            answer=Math.max(answer,sum);
         }
         return answer;
     }
-    //public Node(int y,int x)
-    public static void bfs(int startY,int startX,int number,int[][] land){
-        ch[startY][startX]=number;
-        ArrayDeque<Node> q=new ArrayDeque<>();
-        q.add(new Node(startY,startX));
-        int cnt=0;
+    
+    public static void bfs(int startY,int startX,int idx,int[][] land){
+        ArrayDeque<Position> q=new ArrayDeque<>();
+        q.add(new Position(startY,startX));
+        ch[startY][startX]=idx;
+        int sum=0;
         while(!q.isEmpty()){
-            Node now=q.poll();
+            Position now=q.poll();
             int nowY=now.y;
             int nowX=now.x;
-            cnt++;
+            sum++;
             for(int i=0;i<4;i++){
                 int nextY=nowY+dy[i];
                 int nextX=nowX+dx[i];
-                if(isIn(nextY,nextX,land)){
-                    ch[nextY][nextX]=number;
-                    q.add(new Node(nextY,nextX));
+                if(isIn(nextY,nextX)&&land[nextY][nextX]==1){
+                    ch[nextY][nextX]=idx;
+                    q.add(new Position(nextY,nextX));
                 }
             }
         }
-        map.put(number,cnt);
+        list.add(sum);
     }
-    public static boolean isIn(int y,int x,int[][] land){
-        if(y>=0 && y<N && x>=0 && x<M && land[y][x]==1 && ch[y][x]==0){
+    public static boolean isIn(int y,int x){
+        if(y>=0 && y<N && x>=0 && x<M &&ch[y][x]==0){
             return true;
         }
         return false;
     }
+    
 }
