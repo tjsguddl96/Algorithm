a3939898552d1f3c5334ea8a0cbd31d5dff11ebf
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-10-19 02:49:27 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,33 +1,11 @@
 -- 코드를 입력하세요
-select car_id,(
-    case
-        when car_id in(select car_id
-                      from car_rental_company_rental_history
-                      where date_format(start_date,'%Y-%m-%d')<='2022-10-16' and date_format(end_date,'%Y-%m-%d')>='2022-10-16') then '대여중'
-        else '대여 가능'
-    end
-) as availability
-from car_rental_company_rental_history
-group by car_id
-order by car_id desc
-
-# select car_id,start_date,end_date
-# from car_rental_company_rental_history
-# order by car_id
-
-# select car_id,date_format(start_date,'%Y-%m-%d'),date_format(end_date,'%Y-%m-%d')
-# from car_rental_company_rental_history
-# where (car_id,end_date) in (select car_id,max(end_date)
-#                 from car_rental_company_rental_history
-#                 group by car_id)
-# order by car_id desc
-
-
-
-# select car_id,start_date,end_date
-# from car_rental_company_rental_history
-# where car_id=20
-# order by end_date
-
-
-# from -> where -> group by-> having -> select -> order by 
+select history.car_id CAR_ID,
+case 
+    when CAR_ID NOT IN (SELECT car_id
+                        from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                        where start_date<='2022-10-16' and end_date>='2022-10-16') THEN '대여 가능'
+else '대여중'
+end AVAILABILITY
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY history
+group by CAR_ID
+order by CAR_ID desc
