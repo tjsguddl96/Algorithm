787d933647ca9b6fc0b0dee1886b9e7bedc2a2b5
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-10-25 16:08:54 +0900

    [level 3] Title: 부대복귀, Time: 237.90 ms, Memory: 189 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,25 +1,27 @@
 import java.util.*;
-import java.io.*;
 class Solution {
-    static class Dist{
-        int dist;
+    static int[] dist;
+    static class Node implements Comparable<Node>{
         int n;
-        public Dist(int dist,int n){
-            this.dist=dist;
+        int d;
+        public Node(int n,int d){
             this.n=n;
+            this.d=d;
+        }
+        @Override
+        public int compareTo(Node o){
+            return this.d-o.d;
         }
     }
-    static int[] ch;
-    static ArrayDeque<Dist> q=new ArrayDeque<>();
-    // static int[][] dist;
+    static int INF=Integer.MAX_VALUE;
     static ArrayList<Integer>[] arr;
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        int[] answer = {};
-        ch=new int[n+1];
+        int[] answer = new int[sources.length];
+        dist=new int[n+1];
         arr=new ArrayList[n+1];
-        for(int i=1;i<n+1;i++){
+        for(int i=0;i<n+1;i++){
+            dist[i]=INF;
             arr[i]=new ArrayList<>();
-            ch[i]=-1;
         }
         for(int i=0;i<roads.length;i++){
             int n1=roads[i][0];
@@ -27,30 +29,36 @@ class Solution {
             arr[n1].add(n2);
             arr[n2].add(n1);
         }
-        q.add(new Dist(0,destination));
-        ch[destination]=0;
-        bfs();
-        answer=new int[sources.length];
+        djkstra(destination);
         for(int i=0;i<sources.length;i++){
-            int ans=sources[i];
-            answer[i]=ch[ans];
+            answer[i]=dist[sources[i]];
+            if(answer[i]==INF){
+                answer[i]=-1;
+            }
         }
         return answer;
     }
-    
-    public void bfs(){
-        while(!q.isEmpty()){
-            Dist now=q.poll();
-            int nowN=now.n;
-            int nowD=now.dist;
-            for(int i=0;i<arr[nowN].size();i++){
-                int nextN=arr[nowN].get(i);
-                int nextD=nowD+1;
-                if(ch[nextN]==-1){
-                    ch[nextN]=nextD;
-                    q.add(new Dist(nextD,nextN));
+    public static void djkstra(int destination){
+        dist[destination]=0;
+        PriorityQueue<Node> pq=new PriorityQueue<>();
+        pq.add(new Node(destination,0));
+        while(!pq.isEmpty()){
+            Node now=pq.poll();
+            int nowNode=now.n;
+            int nowD=now.d;
+            if(dist[nowNode]<nowD){
+                continue;
+            }
+            for(int i=0;i<arr[nowNode].size();i++){
+                int nextNode=arr[nowNode].get(i);
+                if(dist[nextNode]>nowD+1){
+                    
+                    dist[nextNode]=nowD+1;
+                    pq.add(new Node(nextNode,dist[nextNode]));
                 }
             }
         }
     }
+    
+    
 }
