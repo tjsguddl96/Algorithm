bf2f0e85f128ea9c357d1a7dfacd86692fb6cfec
tjsguddl96 <58164681+tjsguddl96@users.noreply.github.com>
2024-11-05 03:37:15 +0900

    [level 3] Title: 등대, Time: 69.06 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/README.md
+++ 프로그래머스/3/133500. 등대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등대 - 133500 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133500?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133500) 
 
 ### 성능 요약
 
-메모리: 82.1 MB, 시간: 5.08 ms
+메모리: 115 MB, 시간: 69.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 22일 09:54:54
+2024년 11월 05일 03:37:12
 
 ### 문제 설명
 
--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -1,52 +1,69 @@
 import java.util.*;
+
 class Solution {
-    static int[] light;
+    static class Node{
+        int on;
+        int off;
+        public Node(int on,int off){
+            this.on=on;
+            this.off=off;
+        }
+    }
+    static int st;
+    static int[] ch;
+    static Node[] dp;
+    static int ans=Integer.MAX_VALUE;
     static ArrayList<Integer>[] arr;
-    static int[] incoming;
-    static ArrayDeque<Integer> q=new ArrayDeque<>();
     public int solution(int n, int[][] lighthouse) {
         int answer = 0;
-        light=new int[n+1];
-        incoming=new int[n+1];
+        ch=new int[n+1];
         arr=new ArrayList[n+1];
+        dp=new Node[n+1];
         for(int i=1;i<n+1;i++){
             arr[i]=new ArrayList<>();
+            dp[i]=new Node(-1,-1);
         }
-        for(int i=0;i<lighthouse.length;i++){
-            int n1=lighthouse[i][0];
-            int n2=lighthouse[i][1];
-            arr[n1].add(n2);
-            arr[n2].add(n1);
-            incoming[n1]++;
-            incoming[n2]++;
+        for(int i=0;i<n-1;i++){
+            int a=lighthouse[i][0];
+            int b=lighthouse[i][1];
+            arr[a].add(b);
+            arr[b].add(a);
         }
-        for(int i=1;i<n+1;i++){
-            if(incoming[i]==1){
-                q.add(i);
-                incoming[i]--;
-            }
-        }
-        while(!q.isEmpty()){
-            int now=q.poll();
-            for(int i=0;i<arr[now].size();i++){
-                int next=arr[now].get(i);
-                incoming[next]--;
-                if(incoming[next]==1){
-                    q.add(next);
-                }
-                if(light[now]==0){
-                    light[next]=1;
-                }
-            }
-        }
-        for(int i=1;i<n+1;i++){
-            if(light[i]==1){
-                answer++;
-            }
-        }
-        
-        
+        solve(1);
+        //dp[i].on=i번 킨 경우, dp[i].off=i번 끈 경우
+        answer=Math.min(dp[1].on,dp[1].off);
         return answer;
     }
-    
-}
+    //킨 경우 = Math.min(이전 끈 경우+1)
+    //끈 경우 = Math.min(이전 킨 경우)
+        public static Node solve(int now){
+            ch[now]=1;
+            // If it is a leaf node
+            if (arr[now].size() == 1 && ch[arr[now].get(0)] == 1) {
+                dp[now].on = 1; // On case
+                dp[now].off = 0; // Off case
+
+                return dp[now];
+            }
+            // If already calculated
+            if (dp[now].on != -1 && dp[now].off != -1) {
+
+                return dp[now];
+            }
+            // Calculation
+            int maxOn = 0;
+            int maxOff = 0;
+            for (int i = 0; i < arr[now].size(); i++) {
+                int next = arr[now].get(i);
+                if (ch[next] == 0) { // Proceed if not visited
+                    ch[next]=1;
+                    maxOff+=solve(next).on;
+                    maxOn+= Math.min(dp[next].on,dp[next].off);
+                }
+            }
+            dp[now].on = maxOn+1;
+            dp[now].off = maxOff;
+
+            return dp[now];
+        }
+    }
